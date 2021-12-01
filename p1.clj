648e1dbc4f54b8dc312e6c1735a2d7c590409ded
Raymon Zutekouw <40148684+Raymonzut@users.noreply.github.com>
2021-12-01 18:43:03 +0100

    Extract countIncrements

--- p1.clj
+++ p1.clj
@@ -2,7 +2,10 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
+(defn countIncrements [numbers]
+  (count (filter #(< (first %) (second %))
+                 (drop 1 (map vector (conj numbers 0) numbers)))))
+
 (defn -main []
   (let [numbers (map #(Integer/parseUnsignedInt %) (str/split-lines (slurp "p1.in")))]
-    (count (filter #(< (first %) (second %))
-                   (drop 1 (map vector (conj numbers 0) numbers))))))
+    (println (countIncrements numbers))))
