22bd006d26e315ec87865908e80f244f9c323afa
Raymon Zutekouw <40148684+Raymonzut@users.noreply.github.com>
2021-12-01 18:43:34 +0100

    Complete Day 1 Part 2

--- p1.clj
+++ p1.clj
@@ -6,6 +6,13 @@
   (count (filter #(< (first %) (second %))
                  (drop 1 (map vector (conj numbers 0) numbers)))))
 
+(defn threeWindows [numbers]
+  (let [windows (take-while #(= 3 (count %))
+                            (map-indexed (fn [i nums] (take 3 (drop i nums)))
+                                         (repeat numbers)))]
+    (map (partial reduce +) windows)))
+
 (defn -main []
   (let [numbers (map #(Integer/parseUnsignedInt %) (str/split-lines (slurp "p1.in")))]
-    (println (countIncrements numbers))))
+    (println (countIncrements numbers))
+    (println (countIncrements (threeWindows numbers)))))
