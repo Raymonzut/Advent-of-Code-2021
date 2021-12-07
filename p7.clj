ccb7c4cf705496aa8f0166fd8ce4b0656e4a315d
Raymon Zutekouw <40148684+Raymonzut@users.noreply.github.com>
2021-12-07 12:29:49 +0100

    Complete Day 7

---
+++ p7.clj
@@ -0,0 +1,44 @@
+(ns aoc.p7
+  (:use [aoc.lib :as lib])
+  (:require [clojure.string :as str])
+  (:gen-class))
+
+(def filename "p7.in")
+
+(defn read-input-parsed []
+  (->> filename
+       slurp
+       str/split-lines
+       first
+       (#(str/split % #","))
+       (map #(Integer/parseUnsignedInt %))))
+
+(defn align-cost-basic [positions, alignment]
+  (->> positions
+       (map (fn [p] (Math/abs (- alignment p))))
+       (reduce +)))
+
+(defn align-cost-advanced [positions, alignment]
+  (->> positions
+       (map (fn [p] (Math/abs (- alignment p))))
+       (map (fn [end] (* 0.5 end (inc end))))
+       (reduce +)
+       (Math/round)))
+
+(defn alignments-within [positions]
+  (range (reduce min positions) (inc (reduce max positions))))
+
+(defn minimal-alignment [positions align-cost]
+  (loop [left-alignments (alignments-within positions)
+         min-align 0
+         min-align-cost (align-cost positions (first left-alignments))]
+    (if-let [alignment (first left-alignments)]
+      (if (< (align-cost positions alignment) min-align-cost)
+        (recur (rest left-alignments) alignment (align-cost positions alignment))
+        (recur (rest left-alignments) min-align min-align-cost))
+      min-align-cost)))
+
+(defn -main []
+  (let [input (read-input-parsed)]
+    (println (minimal-alignment input align-cost-basic))
+    (println (minimal-alignment input align-cost-advanced))))
